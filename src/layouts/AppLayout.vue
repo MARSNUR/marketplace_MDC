f492caf1e00d844758b8a1c6faa821e331f9a5df
Nurmars <marsnur7@gmail.com>
2023-05-13 14:21:39 +0600

    first commit

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<template>
+   <AppHeader msg="Something" />
+   <AppMain msg="Welcome to Your Vue.js App" />
+   <AppFooter msg="sdfsdf" />
+   <router-view />
+</template>
+ 
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+import AppMain from '@/components/AppMain.vue';
+import AppFooter from '@/components/AppFooter.vue';
+
+export default {
+   name: 'AppLayouts',
+   components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+   }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import './assets/js/bootstrap-select.min.js'
+import './assets/js/bootstrap-slider.min.js'
+import './assets/js/magnific-popup.min.js'
+import './assets/js/clipboard.min.js'
+import './assets/js/counterup.min.js'
+// import './assets/js/mmenu.min.js'
+import './assets/js/simplebar.min.js'
+import './assets/js/slick.min.js'
+import './assets/js/snackbar.js'
+import './assets/css/style.css';
+
+
+
+createApp(App)
+   .use(store) 
+   .use(router) 
+   .mount('#app')
